43eecc3c29ae5952fc9ad2b2efd346d143165fdd
Markoradoj <G00436648@atu.ie>
2025-04-23 20:05:10 +0000

    added pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,13 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  
+  {
+    path: 'weather',
+    loadComponent: () => import('./weather/weather.page').then( m => m.WeatherPage)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.page').then( m => m.AboutPage)
+  },
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import {IonButtons,IonCol, IonRow, IonGrid, IonButton,IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButton, IonCol, IonRow, IonGrid, IonButton, IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ IonButtons, IonButton, IonCol, IonRow, IonGrid, IonButton, IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class HomePage {
-  constructor() {}
+  constructor(private rotuer: Router) {}
+
+  pageWeather()
+  {
+    this.rotuer.navigate(['/weather']);
+  }
+
+joinPage(){
+  
+}
+
+aboutPage(){
+  this.rotuer.navigate(['/about']);
+}
+
 }
