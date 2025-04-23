c09535275009469990240ea3a4929ca4a2fd3d94
Markoradoj <G00436648@atu.ie>
2025-04-23 18:48:25 +0000

    buttons added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {IonButtons,IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonButtons,IonCol, IonRow, IonGrid, IonButton,IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButtons, IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonCol, IonRow, IonGrid, IonButton, IonBackButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
